6ebe4b9e641e3f81e2ff990ea7c8a6f7bf48b280
swoichha <swoichhaa@gmail.com>
2021-07-05 17:23:16 +0545

    add consmer header file

--- test/pact/pactHelper.cpp
+++ test/pact/pactHelper.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include <consumer.h>
-#include "todo.h"
+#include "consumer/consumer.h"
+#include <pact_mock_server_ffi.h>
 
 using namespace testing;
 using namespace pact_consumer;
@@ -10,45 +8,16 @@ using namespace pact_consumer::matchers;
 
 TEST(PactConsumerTest, GetJsonProjects) {
   auto provider = Pact("client", "oc-server");
-  provider.pact_directory = "tests/pacts";
+  provider.pact_directory = "tests/pact";
 
   provider
     .given("i have a list of projects")
     .uponReceiving("a request for projects")
-    .withRequest("GET", "/projects")
-    .withQuery(query)
-    .withHeaders(headers)
-    .willRespondWith(200)
-    .withResponseHeaders(res_headers)
-    .withResponseJsonBody(Object({
-      { "projects", EachLike(Object({
-        { "id", Integer(3) },
-        { "name", Like("Project 1") },
-        { "due", DateTime("yyyy-MM-dd'T'HH:mm:ss.SSSX") },
-        { "tasks", AtLeastOneLike(4, Object({
-          { "id", Integer() },
-          { "name", Matching("Task \\d+", "Task 1") },
-          { "done", Like(true) }
-        }))}
-      }))}
-    }));
+    .withRequest("GET", "/ocs/v2.php/cloud/users/Alice?format=json")
+    .willRespondWith(200);
 
-      auto result = provider.run_test([] ('http://localhost/core') {
-        TodoClient todo;
-        todo.serverUrl = mock_server->get_url();
-        std::cout << "URL: " << todo.serverUrl << "\n";
-
-        std::vector<Project> projects = todo.getProjects();
-
-        EXPECT_THAT(todo.getProjects(), SizeIs(1));
-        Project p = projects[0];
-        EXPECT_EQ(p.name, "Project 1");
-        EXPECT_GE(p.id, 0);
-        EXPECT_THAT(p.tasks, SizeIs(4));
-        EXPECT_GE(p.tasks[0].id, 0);
-        EXPECT_EQ(p.tasks[0].name, "Task 1");
-
-        return ::testing::UnitTest::GetInstance()->current_test_suite()->Passed();
-      });
-      EXPECT_TRUE(result.is_ok()) << "Test failed";
+  auto result = provider.run_test([] (auto mock_server) {
+    return ::testing::UnitTest::GetInstance()->current_test_suite()->Passed();
+  });
+  EXPECT_TRUE(result.is_ok()) << "Test failed";
 }
